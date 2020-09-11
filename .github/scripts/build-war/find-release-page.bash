29d03838a52129a32628c6917ef2b599c25da810
footaku <18702821+footaku@users.noreply.github.com>
2020-09-11 16:14:02 +0900

    リリース名で指定する

--- .github/scripts/build-war/find-release-page.bash
+++ .github/scripts/build-war/find-release-page.bash
@@ -2,16 +2,19 @@
 
 set -eu
 
-RELEASE_PAGE_URL_PREFIX="https://api.github.com/repos/footaku/scratches/releases/tags"
+RELEASE_PAGE_URL="https://api.github.com/repos/footaku/scratches/releases"
 
-function tag {
+function find-page {
     echo "$(curl -fsL --request GET \
       -H "Accept: application/vnd.github.v3+json" \
       -H "Authorization: Bearer ${1}" \
       -H "Content-Type: application/json" \
-      "${RELEASE_PAGE_URL_PREFIX}/$(printf "${2}" | jq -s -R -r @uri)" |
-      jq -r .id)"
+      "${RELEASE_PAGE_URL}" | jq -r ".[] | select(.name == \"${2}\") | .id")"
+}
+
+function page-name {
+    echo $(printf "${1}" | jq -s -R -r @uri)
 }
 
 ## tagを受け取ってリリースページを探す
-tag ${1} ${2}
+find-page ${1} "$(page-name ${2})"
