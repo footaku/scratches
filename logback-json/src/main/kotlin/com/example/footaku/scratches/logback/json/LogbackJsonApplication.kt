5beda576d210f87b92e1bb241db9cee9b4914949
footaku <18702821+footaku@users.noreply.github.com>
2020-10-12 13:17:49 +0900

    logback-spring

---
+++ logback-json/src/main/kotlin/com/example/footaku/scratches/logback/json/LogbackJsonApplication.kt
@@ -0,0 +1,11 @@
+package com.example.footaku.scratches.logback.json
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogbackJsonApplication
+
+fun main(args: Array<String>) {
+    runApplication<LogbackJsonApplication>(*args)
+}
---
+++ logback-json/src/test/kotlin/com/example/footaku/scratches/logback/json/LogbackJsonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.footaku.scratches.logback.json
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogbackJsonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
